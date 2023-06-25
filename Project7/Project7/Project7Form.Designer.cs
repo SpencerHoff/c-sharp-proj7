9ab783a75f6e76ed4cf1d3ecf7f8a0d2be3bed91
SpencerHoff <137660830+SpencerHoff@users.noreply.github.com>
2023-06-24 20:46:37 -0400

    Add files via upload

---
+++ Project7/Project7/Project7Form.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Project7
+{
+    partial class Project7Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.firstname_box = new System.Windows.Forms.TextBox();
+            this.lastname_box = new System.Windows.Forms.TextBox();
+            this.state_box = new System.Windows.Forms.TextBox();
+            this.customers_label = new System.Windows.Forms.Label();
+            this.firstname_label = new System.Windows.Forms.Label();
+            this.lastname_label = new System.Windows.Forms.Label();
+            this.state_label = new System.Windows.Forms.Label();
+            this.search_button = new System.Windows.Forms.Button();
+            this.exit_button = new System.Windows.Forms.Button();
+            this.lastinitial_box = new System.Windows.Forms.TextBox();
+            this.lastinitial_label = new System.Windows.Forms.Label();
+            this.firstinital_label = new System.Windows.Forms.Label();
+            this.firstinitial_box = new System.Windows.Forms.TextBox();
+            this.save_button = new System.Windows.Forms.Button();
+            this.customer_listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // firstname_box
+            // 
+            this.firstname_box.Location = new System.Drawing.Point(193, 76);
+            this.firstname_box.Name = "firstname_box";
+            this.firstname_box.Size = new System.Drawing.Size(195, 20);
+            this.firstname_box.TabIndex = 0;
+            // 
+            // lastname_box
+            // 
+            this.lastname_box.Location = new System.Drawing.Point(193, 149);
+            this.lastname_box.Name = "lastname_box";
+            this.lastname_box.Size = new System.Drawing.Size(195, 20);
+            this.lastname_box.TabIndex = 1;
+            // 
+            // state_box
+            // 
+            this.state_box.Location = new System.Drawing.Point(193, 220);
+            this.state_box.Name = "state_box";
+            this.state_box.Size = new System.Drawing.Size(195, 20);
+            this.state_box.TabIndex = 2;
+            // 
+            // customers_label
+            // 
+            this.customers_label.AutoSize = true;
+            this.customers_label.Location = new System.Drawing.Point(461, 45);
+            this.customers_label.Name = "customers_label";
+            this.customers_label.Size = new System.Drawing.Size(59, 13);
+            this.customers_label.TabIndex = 4;
+            this.customers_label.Text = "Customers:";
+            // 
+            // firstname_label
+            // 
+            this.firstname_label.AutoSize = true;
+            this.firstname_label.Location = new System.Drawing.Point(49, 76);
+            this.firstname_label.Name = "firstname_label";
+            this.firstname_label.Size = new System.Drawing.Size(60, 13);
+            this.firstname_label.TabIndex = 5;
+            this.firstname_label.Text = "First Name:";
+            // 
+            // lastname_label
+            // 
+            this.lastname_label.AutoSize = true;
+            this.lastname_label.Location = new System.Drawing.Point(49, 149);
+            this.lastname_label.Name = "lastname_label";
+            this.lastname_label.Size = new System.Drawing.Size(61, 13);
+            this.lastname_label.TabIndex = 6;
+            this.lastname_label.Text = "Last Name:";
+            // 
+            // state_label
+            // 
+            this.state_label.AutoSize = true;
+            this.state_label.Location = new System.Drawing.Point(52, 220);
+            this.state_label.Name = "state_label";
+            this.state_label.Size = new System.Drawing.Size(35, 13);
+            this.state_label.TabIndex = 7;
+            this.state_label.Text = "State:";
+            // 
+            // search_button
+            // 
+            this.search_button.Location = new System.Drawing.Point(523, 288);
+            this.search_button.Name = "search_button";
+            this.search_button.Size = new System.Drawing.Size(75, 23);
+            this.search_button.TabIndex = 4;
+            this.search_button.Text = "Search";
+            this.search_button.UseVisualStyleBackColor = true;
+            this.search_button.Click += new System.EventHandler(this.search_button_Click);
+            // 
+            // exit_button
+            // 
+            this.exit_button.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.exit_button.Location = new System.Drawing.Point(667, 387);
+            this.exit_button.Name = "exit_button";
+            this.exit_button.Size = new System.Drawing.Size(75, 23);
+            this.exit_button.TabIndex = 5;
+            this.exit_button.Text = "Exit";
+            this.exit_button.UseVisualStyleBackColor = true;
+            this.exit_button.Click += new System.EventHandler(this.exit_button_Click);
+            // 
+            // lastinitial_box
+            // 
+            this.lastinitial_box.Location = new System.Drawing.Point(429, 392);
+            this.lastinitial_box.Name = "lastinitial_box";
+            this.lastinitial_box.Size = new System.Drawing.Size(133, 20);
+            this.lastinitial_box.TabIndex = 11;
+            this.lastinitial_box.TabStop = false;
+            // 
+            // lastinitial_label
+            // 
+            this.lastinitial_label.AutoSize = true;
+            this.lastinitial_label.Location = new System.Drawing.Point(353, 392);
+            this.lastinitial_label.Name = "lastinitial_label";
+            this.lastinitial_label.Size = new System.Drawing.Size(57, 13);
+            this.lastinitial_label.TabIndex = 12;
+            this.lastinitial_label.Text = "Last Initial:";
+            // 
+            // firstinital_label
+            // 
+            this.firstinital_label.AutoSize = true;
+            this.firstinital_label.Location = new System.Drawing.Point(52, 392);
+            this.firstinital_label.Name = "firstinital_label";
+            this.firstinital_label.Size = new System.Drawing.Size(56, 13);
+            this.firstinital_label.TabIndex = 13;
+            this.firstinital_label.Text = "First Initial:";
+            // 
+            // firstinitial_box
+            // 
+            this.firstinitial_box.Location = new System.Drawing.Point(128, 392);
+            this.firstinitial_box.Name = "firstinitial_box";
+            this.firstinitial_box.Size = new System.Drawing.Size(126, 20);
+            this.firstinitial_box.TabIndex = 14;
+            this.firstinitial_box.TabStop = false;
+            // 
+            // save_button
+            // 
+            this.save_button.Location = new System.Drawing.Point(262, 287);
+            this.save_button.Name = "save_button";
+            this.save_button.Size = new System.Drawing.Size(75, 23);
+            this.save_button.TabIndex = 3;
+            this.save_button.Text = "Save";
+            this.save_button.UseVisualStyleBackColor = true;
+            this.save_button.Click += new System.EventHandler(this.save_button_Click);
+            // 
+            // customer_listbox
+            // 
+            this.customer_listbox.FormattingEnabled = true;
+            this.customer_listbox.Location = new System.Drawing.Point(464, 67);
+            this.customer_listbox.Name = "customer_listbox";
+            this.customer_listbox.Size = new System.Drawing.Size(248, 173);
+            this.customer_listbox.Sorted = true;
+            this.customer_listbox.TabIndex = 16;
+            this.customer_listbox.TabStop = false;
+            // 
+            // Project7Form
+            // 
+            this.AcceptButton = this.save_button;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.exit_button;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customer_listbox);
+            this.Controls.Add(this.save_button);
+            this.Controls.Add(this.firstinitial_box);
+            this.Controls.Add(this.firstinital_label);
+            this.Controls.Add(this.lastinitial_label);
+            this.Controls.Add(this.lastinitial_box);
+            this.Controls.Add(this.exit_button);
+            this.Controls.Add(this.search_button);
+            this.Controls.Add(this.state_label);
+            this.Controls.Add(this.lastname_label);
+            this.Controls.Add(this.firstname_label);
+            this.Controls.Add(this.customers_label);
+            this.Controls.Add(this.state_box);
+            this.Controls.Add(this.lastname_box);
+            this.Controls.Add(this.firstname_box);
+            this.Name = "Project7Form";
+            this.Text = "Spencer Hoff";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox firstname_box;
+        private System.Windows.Forms.TextBox lastname_box;
+        private System.Windows.Forms.TextBox state_box;
+        private System.Windows.Forms.Label customers_label;
+        private System.Windows.Forms.Label firstname_label;
+        private System.Windows.Forms.Label lastname_label;
+        private System.Windows.Forms.Label state_label;
+        private System.Windows.Forms.Button search_button;
+        private System.Windows.Forms.Button exit_button;
+        private System.Windows.Forms.TextBox lastinitial_box;
+        private System.Windows.Forms.Label lastinitial_label;
+        private System.Windows.Forms.Label firstinital_label;
+        private System.Windows.Forms.TextBox firstinitial_box;
+        private System.Windows.Forms.Button save_button;
+        private System.Windows.Forms.ListBox customer_listbox;
+    }
+}
+
---
+++ Project7/Project7/Project7Form.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project7
+{
+    public partial class Project7Form : Form
+    {
+        string firstname;
+        string lastname;
+
+        string upperstate;
+        string firstinitial;
+        string lastinitial;
+        string newstate;
+
+        public Project7Form()
+        {
+            InitializeComponent();
+        }
+        /* this changes all of the characters for the state input to uppercase, then determines if the first character of the first 
+        and last name is uppercase. If so, it places the values into a single string in the customers box*/
+        private void save_button_Click(object sender, EventArgs e)
+        {
+            
+            if (state_box.Text != "")
+            {
+                upperstate = "";
+                foreach (char state in state_box.Text)
+                {
+                    
+                    upperstate = upperstate + char.ToUpper(state);
+                    
+                }
+                MessageBox.Show(upperstate);
+                if (char.IsUpper(firstname_box.Text[0]))
+                {
+                    if (char.IsUpper(lastname_box.Text[0]))
+                    {
+                        customer_listbox.Items.Add(lastname_box.Text + ", " + firstname_box.Text + ", " + upperstate);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please capitalize the last name.");
+                        lastname_box.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please capitalize the first name.");
+                    firstname_box.Focus();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a value for the state.");
+                state_box.Focus();
+
+            }
+            
+            firstname_box.Text = "";
+            lastname_box.Text = "";
+            state_box.Text = "";
+            firstinitial_box.Text = "";
+            lastinitial_box.Text = "";
+            firstname_box.Focus();
+
+        }
+        //this refrences the selection from the customers list and displays the initials of their name
+        private void search_button_Click(object sender, EventArgs e)
+        {
+            if (customer_listbox.SelectedIndex > -1)
+            {
+                string str = customer_listbox.SelectedItem.ToString();
+                str = str.Trim();
+                string[] tokens = str.Split(',');
+
+
+                firstinitial = tokens[1].Trim();
+                firstname_box.Text = firstinitial;
+                string firstinitial2 = firstinitial.Remove(1);
+                firstinitial_box.Text = firstinitial2;
+
+             
+                lastinitial = tokens[0];
+                lastname_box.Text = lastinitial;
+                string lastinitial2 = lastinitial.Remove(1);
+                lastinitial_box.Text = lastinitial2;
+
+
+                newstate = tokens[2].Trim();
+                state_box.Text = newstate;
+            }
+            else 
+            {
+                MessageBox.Show("Please select a name to search.");
+            }
+        }
+        // this closes the form
+        private void exit_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
